d541ec7efd51732e1ac07363b99a39282b79a0d6
smkim <sunmin0406@naver.com>
2023-04-03 14:12:12 +0900

    init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 3000
 
 app.get('/', (req, res) => {
-    res.send('run server')
+    res.send('run server') 
 })
 
 const server = app.listen(port, () => {
