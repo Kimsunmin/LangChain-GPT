6cd2d384532e0748bbf6b108cfbff75bd1e5fd8d
smkim <sunmin0406@naver.com>
2023-04-03 14:58:01 +0900

    pinecone init

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from 'express'
+import express from 'express';
+import { pinecone } from './utils/pinecone-client.js';
+
 const app = express()
 const port = process.env.port || 3000
 
 app.get('/', (req, res) => {
-    res.send('run server') 
+    res.send('run server')
 })
 
 const server = app.listen(port, () => {
+    console.log(process.env)
     console.log(`server on ${port}`)
 })
